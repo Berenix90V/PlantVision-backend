2c05c1722d36e19115012639372959915cae88bb
Cristian Nicolae Lupascu <cristiannicolae.lupascu@edu.turkuamk.fi>
2022-04-22 09:55:03 +0300

    Added configuration file

---
+++ backend/src/config/config.ts
@@ -0,0 +1,7 @@
+export const config =  {
+    CONNECTION_URL_REMOTE:"mongodb+srv://cristian:cristian@cluster0.xtqwj.mongodb.net/Plants?retryWrites=true&w=majority",
+    CONNECTION_URL_DOCKER:"http://database:27017",
+    DB_USERNAME:"cristian",
+    DB_PASSWORD:"cristian",
+    SERVER_PORT:5000
+}
