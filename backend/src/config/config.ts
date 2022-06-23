e60c8a854dfc1aecae761e07e723f21f22a9c0db
Veronica Zanella <64438184+Berenix90V@users.noreply.github.com>
2022-06-23 21:07:27 +0200

    Update config.ts

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,5 +1,4 @@
 export const config =  {
-    CONNECTION_URL_REMOTE:"mongodb+srv://cristian:cristian@cluster0.xtqwj.mongodb.net/Plants?retryWrites=true&w=majority",
     CONNECTION_URL_DOCKER:"mongodb://database:27017",
     DB_USERNAME:"cristian",
     DB_PASSWORD:"cristian",
